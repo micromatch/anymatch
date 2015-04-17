4d871c487d2d79a59a3b38d6857c5eb5ebd73d5a
Elan Shanker <elan.shanker@gmail.com>
2015-04-17 19:03:51 -0400

    Resolve backslashes on win32
    For string and regex matchers. Globs are already taken care of by
    micromatch ^2.1.5

--- index.js
+++ index.js
@@ -3,26 +3,40 @@
 var micromatch = require('micromatch');
 var arrify = require('arrify');
 
+var platform = process.platform;
+
 var anymatch = function(criteria, value, returnIndex, startIndex, endIndex) {
   criteria = arrify(criteria);
   value = arrify(value);
   if (arguments.length === 1) {
     return anymatch.bind(null, criteria.map(function(criterion) {
-      return typeof criterion === 'string' ?
-        micromatch.matcher(criterion) : criterion;
+      if (typeof criterion !== 'string') {
+        return criterion;
+      }
+      var matcher = micromatch.matcher(criterion);
+      return function(string) {
+        return matcher(string) ||
+          platform === 'win32' && matcher(string.split('\\').join('/'));
+      };
     }));
   }
   startIndex = startIndex || 0;
   var string = value[0];
+  var altString;
+  if (platform === 'win32' && typeof string === 'string') {
+    altString = string.split('\\').join('/');
+    altString = altString === string ? null : altString;
+  }
   var matchIndex = -1;
   function testCriteria (criterion, index) {
     var result;
     switch (toString.call(criterion)) {
     case '[object String]':
-      result = string === criterion || micromatch.isMatch(string, criterion);
+      result = string === criterion || altString && altString === criterion;
+      result = result || micromatch.isMatch(string, criterion);
       break;
     case '[object RegExp]':
-      result = criterion.test(string);
+      result = criterion.test(string) || altString && criterion.test(altString);
       break;
     case '[object Function]':
       result = criterion.apply(null, value);
