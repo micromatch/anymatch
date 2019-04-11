cd0df7c5c65c5ef7dd5e5fed751155dc732228aa
Paul Miller <paul@paulmillr.com>
2019-04-11 10:14:32 +0200

    Fix typess.

--- index.d.ts
+++ index.d.ts
@@ -5,6 +5,8 @@ import sep from 'path';
 type AnymatchFn = (string) => boolean;
 type AnymatchPattern = string|RegExp|AnymatchFn;
 type AnymatchMatcher = AnymatchPattern|Array<AnymatchPattern>
-declare function anymatch(matchers: AnymatchMatcher, testString: string|Array, returnIndex?: boolean): boolean;
-declare function anymatch(matchers: AnymatchMatcher): (testString: string|Array, returnIndex?: boolean) => number;
+declare function anymatch(matchers: AnymatchMatcher, testString: string|Array<any>): boolean;
+declare function anymatch(matchers: AnymatchMatcher, testString: string|Array<any>, returnIndex: true): number;
+declare function anymatch(matchers: AnymatchMatcher): (testString: string|Array<any>) => boolean;
+declare function anymatch(matchers: AnymatchMatcher): (testString: string|Array<any>, returnIndex: true) => number;
 export = anymatch;
